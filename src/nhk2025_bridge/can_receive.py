365a5121f319a5e4ec0bacc95ce25351b750b96d
aky <akiyugata271@gmail.com>
2025-05-09 18:32:01 +0900

    receive practice

--- src/nhk2025_bridge/can_receive.py
+++ src/nhk2025_bridge/can_receive.py
@@ -1,8 +1,17 @@
-import rclpy
-from rclpy.node import Node
+#import rclpy
+#from rclpy.node import Node
 
 import can
 
-with can.Bus() as bus:
-    for msg in bus:
-        print(msg.data)
+# Connect to the CAN bus using python-can
+can0 = can.interface.Bus(channel='can0', interface='socketcan')
+
+try:
+    while True:
+        msg = can0.recv()
+        print(msg)
+        print('run')
+except KeyboardInterrupt:
+    pass
+finally:
+    can0.shutdown()
