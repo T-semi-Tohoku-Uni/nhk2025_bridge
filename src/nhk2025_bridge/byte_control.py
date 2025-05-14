3275983cff40589d5bd4edcb192021aa499c7c76
aky <akiyugata271@gmail.com>
2025-05-14 14:27:47 +0900

    create bridge

--- src/nhk2025_bridge/byte_control.py
+++ src/nhk2025_bridge/byte_control.py
@@ -11,12 +11,3 @@ class ValueBridge():
         f32_ndarray = np.frombuffer(byte, dtype=np.float32)
         f32 = float(f32_ndarray[0])
         return f32
-
-if __name__ == '__main__':
-    bridge = ValueBridge()
-    f32_test = 100
-    byte_test = bridge.f32_to_byte(f32_test)
-    f32_test_test = bridge.byte_to_f32(byte_test)
-    print(f32_test)
-    print(byte_test)
-    print(f32_test_test)
