cc830d4d9b1df956d9da2d1811199c88c45ff9ad
aky <akiyugata271@gmail.com>
2025-05-15 01:30:30 +0900

    デバッグのためのファイル、変更を削除した

--- src/nhk2025_bridge/debug_byte_control.py
+++
@@ -1,8 +0,0 @@
-from nhk2025_bridge.byte_control import ValueBridge
-
-def main():
-    bridge = ValueBridge()
-    f32_list = [32, 20.78, 45.33]
-    byte_list = bridge.nhk2025_f32_to_byte(f32_list)
-    f32_list_return = bridge.nhk2025_byte_to_f32(byte_list)
-    print(f32_list_return)
