037e61166bc5dc8818d765accc0625b8d2c4e0e6
aky <akiyugata271@gmail.com>
2025-05-15 01:00:59 +0900

    デバッグのための準備をした

---
+++ scripts/debug_byte_control.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from nhk2025_bridge import main
+
+if __name__ == '__main__':
+    main()
--- src/nhk2025_bridge/__init__.py
+++ src/nhk2025_bridge/__init__.py
@@ -1,4 +1,5 @@
 from nhk2025_bridge.can_receive import main_canbridge
 from nhk2025_bridge.can_send import main_can_send
+from nhk2025_bridge.debug_byte_control import main
 def hello() -> str:
     return "Hello from nhk2025-bridge!"
