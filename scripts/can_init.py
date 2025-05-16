aeb99d48167e250c6c6d3d5e31b9ac914e9da867
aky <akiyugata271@gmail.com>
2025-05-16 20:17:10 +0900

    can_init

---
+++ scripts/can_init.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from nhk2025_bridge import can_init
+
+if __name__ == '__main__':
+    can_init()
--- src/nhk2025_bridge/__init__.py
+++ src/nhk2025_bridge/__init__.py
@@ -1,5 +1,6 @@
 from nhk2025_bridge.can_bridge import main_canbridge
 from nhk2025_bridge.ros2_to_can import main_ros2_to_can
 from nhk2025_bridge.can_monitoring import main_can_monitor
+from nhk2025_bridge.scripts.can_init import can_init
 def hello() -> str:
     return "Hello from nhk2025-bridge!"
