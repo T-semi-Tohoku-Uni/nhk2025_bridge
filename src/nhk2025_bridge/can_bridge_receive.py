8cbc67b8784ef091f22bd1eedd160fcb5541e74e
aky <akiyugata271@gmail.com>
2025-05-18 14:27:54 +0900

    canのモニタリングをするノード

---
+++ src/nhk2025_bridge/can_bridge_receive.py
@@ -0,0 +1,33 @@
+import rclpy
+from rclpy.node import Node
+
+from nhk2025_custom_message.msg import CanFd
+
+import can
+
+class CanReceive(Node):
+    def __init__(self):
+        super().__init__('can_receive')
+        self.can0 = can.interface.Bus(channel='can0', bustype='socketcan', bitrate=1000000, fd=True, data_bitrate=2000000)
+        self.publisher_can = self.create_publisher(
+            CanFd,
+            'can_msg',
+            10
+        )
+
+    def candata_to_ros2(self, canmsg:can.Message):
+        canfd = CanFd()
+        canfd.id = canmsg.arbitration_id
+        canfd.dlc = canmsg.dlc
+        canfd.data = canmsg.data
+        return canfd
+
+    def __call__(self):
+        while True:
+            msg = self.can0.recv()
+            txdata = self.candata_to_ros2(msg)
+            self.publisher_can.publish(txdata)
+
+def main_can_receive():
+    rclpy.init()
+    
