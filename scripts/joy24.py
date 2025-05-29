34878bef0ddb88c4e85f8c14f611448bbed59c74
aky <akiyugata271@gmail.com>
2025-05-29 22:34:06 +0900

    これから書くだけ

---
+++ scripts/joy24.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from nhk2025_bridge import main_24
+
+if __name__ == '__main__':
+    main_24()
--- src/nhk2025_bridge/__init__.py
+++ src/nhk2025_bridge/__init__.py
@@ -1,6 +1,3 @@
-from nhk2025_bridge.can_to_ros2 import main_can_to_ros2
-from nhk2025_bridge.ros2_to_can import main_ros2_to_can
-from nhk2025_bridge.can_monitoring import main_can_monitor
-from nhk2025_bridge.scripts.can_init import can_init
+from nhk2025_bridge.joy24 import main_24
 def hello() -> str:
     return "Hello from nhk2025-bridge!"
