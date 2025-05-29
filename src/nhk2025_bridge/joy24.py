7a64485bf6975ec47e3de6ea9fd44eaa620bef6f
aky <akiyugata271@gmail.com>
2025-05-29 22:36:21 +0900

    websocketを追加

--- src/nhk2025_bridge/joy24.py
+++ src/nhk2025_bridge/joy24.py
@@ -2,6 +2,35 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 
+import roslibpy
+import time
+
+class WebSocketClientJoy():
+    def __init__(self, namespace:str, host:str, port:int):
+        self.client = roslibpy.Ros(host=host, port=port)
+        self.client.run()
+        print("接続成功")
+        self.joy_publisher = roslibpy.Topic(self.client, namespace + '/joy', 'sensor_msgs/msg/Joy')
+        self.joy_msg = roslibpy.Message()
+
+    def send(self, axes:list, buttons:list):
+        now = time.time()
+        sec = int(now)
+        nanosec = int((now-sec) * 1e9)
+        joy_msg = roslibpy.Message({
+            'header': {
+                'stamp': {'sec': sec, 'nanosec': nanosec},
+                'frame_id': 'joy'
+            },
+            'axes': axes,  
+            'buttons': buttons
+        })
+        self.joy_publisher.publish(joy_msg)
+
+    def destroy_client(self):
+        self.joy_publisher.unadvertise()
+        self.client.terminate()
+        
 class WebsocketClient(Node):
     def __init__(self):
         super().__init__('websocket_client')
