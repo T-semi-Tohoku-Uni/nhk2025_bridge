589e04943291fe7d4faca113e2308a12743183cd
aky <akiyugata271@gmail.com>
2025-06-01 17:15:14 +0900

    実行ファイルを作成

---
+++ scripts/stamp_monitor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from nhk2025_bridge import main_stamp_monitor
+
+if __name__ == '__main__':
+    main_stamp_monitor()
--- src/nhk2025_bridge/__init__.py
+++ src/nhk2025_bridge/__init__.py
@@ -3,5 +3,6 @@ from nhk2025_bridge.ros2_to_can import main_ros2_to_can
 from nhk2025_bridge.can_monitoring import main_can_monitor
 from nhk2025_bridge.scripts.can_init import can_init
 from nhk2025_bridge.stamp_now import main_stamp
+from  nhk2025_bridge.stamp_monitor import main_stamp_monitor
 def hello() -> str:
     return "Hello from nhk2025-bridge!"
